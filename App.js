42757d5835509da99b6007b1b06a8df103169759
BEUMGOO KIM <captain_kbg@naver.com>
2022-01-02 18:01:12 +0900

    get weather data

--- App.js
+++ App.js
@@ -1,16 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, ScrollView, Dimensions ,Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { StyleSheet, ScrollView, ActivityIndicator, Dimensions ,Text, View } from 'react-native';
+import * as Location from 'expo-location';
+import { API_KEY } from '@env';
 
 const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
-
 export default function App() {
+  const [city, setCity] = useState("Loading...");
+  const [days, setDays] = useState([]);
+  const [ok, setOk] = useState(true);
+
+  const getWeather = async () => {
+    const { granted } = await Location.requestForegroundPermissionsAsync();
+    if (!granted) {
+      setOk(false);
+      return;
+    }
+    const {
+      coords: { latitude, longitude },
+    } = await Location.getCurrentPositionAsync({ accuracy: 5 });
+    const location = await Location.reverseGeocodeAsync(
+      { latitude, longitude },
+      { useGoogleMaps: false }
+    );
+    setCity(location[0].city);
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=alerts&appid=${API_KEY}&units=metric`
+    );
+    const json = await response.json();
+    setDays(json.daily);
+  };
+  useEffect(() => {
+    getWeather();
+  }, []);
   return (
     // defaltがflexになっている。flexDirectionはcolume
     <View style={styles.container}>
       <StatusBar style="light"/>
       <View style={styles.city}>
-        <Text style={styles.cityName}>Seoul</Text>
+        <Text style={styles.cityName}>{city}</Text>
       </View>
       <ScrollView
         pagingEnabled
@@ -18,26 +47,25 @@ export default function App() {
         showsHorizontalScrollIndicator={false}
         contentContainerStyle={styles.weather}
       >
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
+        {days.length === 0 ? (
+          <View style={styles.day}>
+            <ActivityIndicator
+              color="white"
+              style={{ marginTop: 10 }}
+              size="large"
+            />
+          </View>
+        ) : (
+          days.map((day, index) => (
+            <View key={index} style={styles.day}>
+              <Text style={styles.temp}>
+                {parseFloat(day.temp.day).toFixed(1)}
+              </Text>
+              <Text style={styles.description}>{day.weather[0].main}</Text>
+              <Text style={styles.tinyText}>{day.weather[0].description}</Text>
+            </View>
+          ))
+        )}
       </ScrollView>
     </View>
   );
@@ -71,5 +99,8 @@ const styles = StyleSheet.create({
   description: {
     marginTop: -30,
     fontSize: 50
-  }
+  },
+  tinyText: {
+    fontSize: 20,
+  },
 });
